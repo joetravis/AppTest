59a9770d11a7141fb8e72e1624eb46b3f513d606
joetravis <joe.travis@adp.com>
2014-02-03 17:54:02 -0800

    Adding basic map functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,9 +12,48 @@
 // MyCtrl2.$inject = [];
 
 angular.module('myApp.controllers', []).
-	controller('MyCtrl1', function() {
+	controller('MyCtrl1', ['$scope', '$window', function($scope, $window) {
+        $scope.centerMap = function (position) {
+            $scope.map.center.latitude = position.coords.latitude;
+            $scope.map.center.longitude = position.coords.longitude;
+            $scope.addMarker(position.coords);
+        };
 
-	}).
+        $scope.addMarker = function (coords) {
+            $scope.map.markers.push({
+                latitude: coords.latitude,
+                longitude: coords.longitude,
+                options: {
+                    visible: true
+                }
+            });
+        };
+
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            options: {
+                panControl: true
+            },
+            zoom: 8,
+            markers: [],
+            events: {
+                click: function(a, b, c) {
+                    console.log(a);
+                    console.log(b);
+                    console.log(c);
+                }
+            }
+        };
+
+        $scope.resetCenter = function() {
+            $scope.map.center.latitude = 45;
+            $scope.map.center.longitude = -73;
+        };
+        $window.navigator.geolocation.getCurrentPosition($scope.centerMap);
+	}]).
 	
 	controller('MyCtrl2', function() {
 
